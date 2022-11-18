3f3b1f051bb1f2f9cea7b715b00fb5bc763799d9
Xheldon <c1006044256@gmail.com>
2022-11-18 18:18:31 +0800

    Initial Commit

---
+++ zxp-installer/zxp_installerApp.swift
@@ -0,0 +1,17 @@
+//
+//  zxp_installerApp.swift
+//  zxp-installer
+//
+//  Created by caoxudong on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct zxp_installerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
